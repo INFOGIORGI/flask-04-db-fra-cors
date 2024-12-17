8ff15b4255d10d3c0d9d8e62ff755849ad42fa26
Francesco Corsino <113785186+fra-cors@users.noreply.github.com>
2024-12-17 11:47:02 +0000

    17/12 da finire

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_templates
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = "138.41.20.102"
+app.config['MYSQL_PORT'] = "53306"
+app.config['MYSQL_DB'] = "w3schools"
+app.config['MYSQL_USER'] = "ospite"
+app.config['MYSQL_PASSWORD'] = "ospite"
+
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_templates("home.html", titolo = home)
+
+def products():
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM prducts"
+    cursor.execute(query)
+    dati = cursor.fetchall
+    
+    return render_templates("products.html", titolo = products)
 
 app.run()
