00bdad7f33861b1d3f153b8e962b1bea0b91a32b
Francesco Corsino <113785186+fra-cors@users.noreply.github.com>
2025-01-06 22:19:17 +0000

    7/01 FINITO

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 from flask_mysqldb import MySQL
-
 
 app = Flask(__name__)
 
+
+#Connessione al Database
 app.config['MYSQL_HOST'] = "138.41.20.102"
-app.config['MYSQL_PORT'] = "53306"
-app.config['MYSQL_DB'] = "w3schools"
+app.config['MYSQL_PORT'] = 53306  #Deve essere intero
 app.config['MYSQL_USER'] = "ospite"
 app.config['MYSQL_PASSWORD'] = "ospite"
+app.config['MYSQL_DB'] = "w3schools"
 
-
+#Creazione oggetto mysql
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
-    return render_templates("home.html", titolo = home)
+    return render_template("home.html", titolo="Home")
 
+@app.route("/products")
 def products():
-    cursor = mysql.connection.cursor()
-    query = "SELECT * FROM prducts"
-    cursor.execute(query)
-    dati = cursor.fetchall
     
-    return render_templates("products.html", titolo = products)
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM products"
+    cursor.execute(query)
+    dati = cursor.fetchall()
+    cursor.close()
 
-app.run()
+    return render_template("products.html", titolo="Products", dati=dati)
+
+@app.route("/products_c/<int:categoryID>")  
+def products_c(categoryID):
+
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM categories"
+    cursor.execute(query)
+    dati = cursor.fetchall()
+    
+    lista=[]
+    for i in dati:
+        if int(i[0]) == categoryID:
+            lista.append(i)
+
+    cursor.close()
+    return render_template("products_c.html", titolo="Categories", categoryID=categoryID, lista=lista)
+
+ 
+app.run(debug=True)
